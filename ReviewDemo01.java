901ef3abb47fce513ae0fe23073a33a55ace4c5f
seunggyuc <seunggyu.choi@inh.edu>
2021-09-11 18:58:34 +0900

    v.03

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -18,23 +18,45 @@
 //    }
 //}
 
+//import java.util.Scanner;
+//// v.02
+//public class ReviewDemo01 {
+//    public static void main(String[] args) {
+//        // 소수 : 1과 자기자신외에는 나누어 떨어지지않는 수
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        int cnt = 0;
+//
+//        for(int k=2; k<number; k++){ // 반복횟수 2회 감소
+//            if(number % k == 0)
+//                cnt++;
+//        }
+//
+//        if(cnt==0) // 카운트가 0 이 유지되면 true
+//            System.out.println(number + "은(는) 소수!");
+//        else System.out.println(number + "은(는) 소수가 아닙니다.");
+//    }
+//}
+
+
 import java.util.Scanner;
-// v.02
+// v.03
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지않는 수
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int cnt = 0;
+        boolean isPrime = true;  // flag 타입의 변수 (isPrime)
+        // is 가 사용되는 구문은 참,거짓을 주로 나타내며, 논리형 boolean 타입을 사용함
 
         for(int k=2; k<number; k++){ // 반복횟수 2회 감소
-            if(number % k == 0)
-                cnt++;
+            if(number % k == 0) // 입력된 수를 k로 나누었을 때 나머지가 0인 경우 즉, 약수의미
+                isPrime  = false;
         }
 
-        if(cnt==0) // 카운트가 0 이 유지되면 true
+        if(isPrime) // isPrime 변수값이 true 로 유지가 되면
             System.out.println(number + "은(는) 소수!");
-        else System.out.println(number + "은(는) 소수가 아닙니다.");
+        else
+            System.out.println(number + "은(는) 소수가 아닙니다.");
     }
 }
-
